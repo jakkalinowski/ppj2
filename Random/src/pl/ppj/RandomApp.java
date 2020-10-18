825d9b64b18de501053b880997148de56b890a66
jakkalinowski <kalinowski@op.pl>
2020-10-18 10:50:58 +0200

    comment

---
+++ Random/src/pl/ppj/RandomApp.java
@@ -0,0 +1,37 @@
+package pl.ppj;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class RandomApp {
+	public static void main(String[] args) {
+
+		int[][] table = new int[50][2];
+		Random rand = new Random();
+		int counter = 0;
+		int empty = 0;
+		int attempt = 20000;
+		
+		do {
+			for (int i = 0; i < table.length; i++) {
+				for (int j = 0; j < table[i].length; j++) {
+					if (j == 0)
+						table[i][j] = i;
+					else if (i == rand.nextInt(attempt) && j == 1)
+						table[i][j] += 1;
+				}
+			}
+			counter++;
+		} while (counter < attempt-1);
+
+		System.out.println(Arrays.deepToString(table));
+		
+		for (int i = 0; i < table.length; i++) {
+			for (int j = 0; j < table[i].length; j++) {
+				if (table[i][j]==0) empty++;
+			}
+		}
+		System.out.println("Numbers not drawn: " + empty * 1. / table.length );
+	}
+
+}
